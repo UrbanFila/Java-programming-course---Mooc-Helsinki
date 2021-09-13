bb7b84932da89accaf40f83f7a0baae007565c3a
UrbanFila <90254842+UrbanFila@users.noreply.github.com>
2021-09-13 17:12:11 +0100

    Create SumOfTwoNumbers.java

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,18 @@
+Import java.util.Scanner;
+
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+System.out.println("Give the first number:");
+int first = Integer.valueOf(scanner.nextLine());
+System.out.println("Give the second number:");
+int second = Integer.valueOf(scanner.nextLine());
+int vsota = first + second;
+System.out.println("The sum of the numbers is "+vsota);
+
+
+    }
+}
