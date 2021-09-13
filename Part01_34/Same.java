9565f39e38c7edc0c05bb23466f7b4768c6b206d
UrbanFila <90254842+UrbanFila@users.noreply.github.com>
2021-09-13 20:50:11 +0100

    Create Same.java

---
+++ Part01_34/Same.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Same {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give the first string:");
+        String word1 = scan.nextLine();
+        System.out.println("Give the second string:");
+        String word2 = scan.nextLine();
+        
+        if(word1.equals(word2)){
+            System.out.println("Same");
+        } else{
+            System.out.println("Different");
+        }
+        
+        // Write your program here. 
+    }
+}
